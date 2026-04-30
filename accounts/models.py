a89c3c5857d517652a3b7b87db3e71b4b70fed36
himabindu-1614 <himabindu16@gmail.com>
2026-05-01 00:31:27 +0530

    fix password hashing

--- accounts/models.py
+++ accounts/models.py
@@ -1,8 +1,16 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.contrib.auth.hashers import make_password
 
 class User(AbstractUser):
     is_admin = models.BooleanField(default=False)
 
     def __str__(self):
         return self.username
+
+
+
+    def save(self, *args, **kwargs):
+        if not self.password.startswith('pbkdf2_'):
+            self.password = make_password(self.password)
+        super().save(*args, **kwargs)
